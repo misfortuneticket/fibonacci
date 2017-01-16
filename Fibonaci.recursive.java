9825f2644993a56abe77d58886417da68e81e1f0
misfortuneticket <misfortuneticket@gmail.com>
2017-01-16 10:35:59 -0500

    Add files via upload

---
+++ Fibonaci.recursive.java
@@ -0,0 +1,27 @@
+
+package fibonacci;
+
+
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b = 1;
+        int i= 0;
+        int j = 0;
+        System.out.println(1);
+
+        private static int fibonacci(int n){
+        if (n>1){
+            n=n+fibonacci(n-1);
+            return(n);   
+            System.out.println(n);
+
+        }
+        return(1);
+   }
+
+    
+}
